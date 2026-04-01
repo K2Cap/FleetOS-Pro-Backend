54212018c2b4da1c65b7472775a9f95b4876c63a
Gravity Logistics <admin@gravitylogistics.com>
2026-04-01 18:58:51 +0530

    Fix multer storage engine

--- server.js
+++ server.js
@@ -455,7 +455,7 @@ const db = {
 const UPLOADS_DIR = path.join(__dirname, 'uploads');
 if (!fs.existsSync(UPLOADS_DIR)) fs.mkdirSync(UPLOADS_DIR);
 
-const storage = createTempMulterStorage(UPLOADS_DIR);
+const storage = multer.diskStorage(createTempMulterStorage(UPLOADS_DIR));
 
 const upload = multer({
     storage,
